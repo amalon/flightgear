amalon/flightgear

commit 24cb7b2d30797109997dd6da08e56dfff0d91a27

VR: Implement a VRManager class
Implement a new VRManager class based on osgXR::Manager to implement its
callbacks. When osgXR needs a new view created, we build a new camera in
the default camera group, and notify osgXR of each new "scene" typed
render pass. It also hooks into the CameraInfo's new compositor reload
callback to ensure osgXR is updated when the compositors are reloaded.

Since we don't use osgXR::setupViewerDefaults() any longer, VR settings
are now controlled by properties instead of environment variables, and
new --enable-vr / --disable-vr options are implemented to start VR
safely at start.

Currently we don't properly implement dynamic start up or shutdown of
VR.

src/Viewer/VRManager.hxx
@@ -1,0 +1,112 @@
+// Copyright (C) 2021  James Hogan
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#ifndef VRMANAGER_HXX
+#define VRMANAGER_HXX 1
+
+#include <config.h>
+
+#ifdef ENABLE_OSGXR
+
+#include <osg/ref_ptr>
+#include <osg/observer_ptr>
+
+#include <osgXR/Manager>
+
+#include <simgear/scene/viewer/CompositorPass.hxx>
+
+#include "CameraGroup.hxx"
+
+#include <map>
+
+#endif
+
+namespace flightgear
+{
+
+#ifdef ENABLE_OSGXR
+
+class VRManager : public osgXR::Manager
+{
+    public:
+
+        class ReloadCompositorCallback : public CameraInfo::ReloadCompositorCallback
+        {
+            public:
+
+                ReloadCompositorCallback(VRManager *manager) :
+                    _manager(manager)
+                {
+                };
+
+                virtual void preReloadCompositor(CameraGroup *cgroup, CameraInfo *info)
+                {
+                    _manager->preReloadCompositor(cgroup, info);
+                }
+
+                virtual void postReloadCompositor(CameraGroup *cgroup, CameraInfo *info)
+                {
+                    _manager->postReloadCompositor(cgroup, info);
+                }
+
+            protected:
+
+                osg::observer_ptr<VRManager> _manager;
+        };
+
+        VRManager();
+
+        static VRManager *instance();
+
+        void resetProperties();
+
+        bool getEnabled() const;
+        void setEnabled(bool enabled);
+
+        const char * getMirrorMode() const;
+        void setMirrorMode(const char * mode);
+
+        const char * getVRMode() const;
+        void setVRMode(const char * mode);
+
+        const char * getSwapchainMode() const;
+        void setSwapchainMode(const char * mode);
+
+        virtual void doCreateView(osgXR::View *xrView);
+        virtual void doDestroyView(osgXR::View *xrView);
+
+        void preReloadCompositor(CameraGroup *cgroup, CameraInfo *info);
+        void postReloadCompositor(CameraGroup *cgroup, CameraInfo *info);
+
+    protected:
+        bool _enabled;
+
+        typedef std::map<osgXR::View *, osg::ref_ptr<CameraInfo>> XRViewToCamInfo;
+        XRViewToCamInfo _camInfos;
+
+        typedef std::map<CameraInfo *, osg::ref_ptr<osgXR::View>> CamInfoToXRView;
+        CamInfoToXRView _xrViews;
+
+        osg::ref_ptr<ReloadCompositorCallback> _reloadCompositorCallback;
+};
+
+#endif // ENABLE_OSGXR
+
+void VRResetProperties();
+
+}
+
+#endif

scripts/completion/fg-completion.zsh
@@ -60,6 +60,8 @@ _fgfs_options=(
 	'--enable-skyblend[Enable sky blending]' \
 	'--disable-textures[Disable textures]' \
 	'--enable-textures[Enable textures]' \
+        '--disable-vr[Disable VR]' \
+        '--enable-vr[Enable VR]' \
 	'--disable-wireframe[Disable wireframe drawing mode]' \
 	'--enable-wireframe[Enable wireframe drawing mode]' \
 	'--notrim[Do NOT attempt to trim the model (only with fdm=jsbsim)]' \

src/Main/options.cxx
@@ -1900,6 +1900,10 @@ struct OptionDesc {
     {"disable-wireframe",            false, OPTION_BOOL,   "/sim/rendering/wireframe", false, "", 0 },
     {"enable-wireframe",             false, OPTION_BOOL,   "/sim/rendering/wireframe", true, "", 0 },
     {"materials-file",               true,  OPTION_STRING, "/sim/rendering/materials-file", false, "", 0 },
+#ifdef ENABLE_OSGXR
+    {"disable-vr",                   false, OPTION_BOOL,   "/sim/vr/enabled", false, "", 0 },
+    {"enable-vr",                    false, OPTION_BOOL,   "/sim/vr/enabled", true, "", 0 },
+#endif
     {"disable-terrasync",            false, OPTION_BOOL,   "/sim/terrasync/enabled", false, "", 0 },
     {"enable-terrasync",             false, OPTION_BOOL,   "/sim/terrasync/enabled", true, "", 0 },
     {"terrasync-dir",                true,  OPTION_IGNORE, "", false, "", 0 },

src/Viewer/CMakeLists.txt
@@ -28,7 +28,14 @@ set(HEADERS
 	viewmgr.hxx
 	sview.hxx
 	GraphicsPresets.hxx
+	VRManager.hxx
 	)
+
+if (ENABLE_OSGXR)
+	list(APPEND SOURCES
+		VRManager.cxx
+		)
+endif()
 
 if (YES)
     list(APPEND HEADERS PUICamera.hxx)

src/Viewer/VRManager.cxx
@@ -1,0 +1,255 @@
+// Copyright (C) 2021  James Hogan
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as
+// published by the Free Software Foundation; either version 2 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#include "VRManager.hxx"
+#include "WindowBuilder.hxx"
+#include "renderer.hxx"
+
+#include <osgXR/Settings>
+
+#include <simgear/scene/viewer/CompositorPass.hxx>
+
+#include <Main/fg_props.hxx>
+#include <Main/globals.hxx>
+
+namespace flightgear
+{
+
+VRManager::VRManager() :
+    _enabled(false),
+    _reloadCompositorCallback(new ReloadCompositorCallback(this))
+{
+    uint32_t fgVersion = (FLIGHTGEAR_MAJOR_VERSION << 16 |
+                          FLIGHTGEAR_MINOR_VERSION << 8 |
+                          FLIGHTGEAR_PATCH_VERSION);
+    _settings->setApp("FlightGear", fgVersion);
+    _settings->preferEnvBlendMode(osgXR::Settings::OPAQUE);
+}
+
+VRManager *VRManager::instance()
+{
+    static osg::ref_ptr<VRManager> single = new VRManager;
+    return single;
+}
+
+void VRManager::resetProperties()
+{
+    fgTie("/sim/vr/present",
+          _settings.get(), &osgXR::Settings::present );
+    fgTie("/sim/vr/openxr/layers/validation",
+          _settings.get(), &osgXR::Settings::hasValidationLayer );
+    fgTie("/sim/vr/openxr/extensions/depth-info",
+          _settings.get(), &osgXR::Settings::hasDepthInfoExtension );
+    fgTie("/sim/vr/openxr/runtime/name",
+          _settings.get(), &osgXR::Settings::runtimeName );
+
+    fgTie("/sim/vr/openxr/system/name",
+          static_cast<osgXR::Manager*>(this), &osgXR::Manager::getSystemName );
+    fgTie("/sim/vr/enabled",
+          this, &VRManager::getEnabled, &VRManager::setEnabled );
+    fgTie("/sim/vr/mirror-mode",
+          this, &VRManager::getMirrorMode, &VRManager::setMirrorMode );
+    fgTie("/sim/vr/mode",
+          this, &VRManager::getVRMode, &VRManager::setVRMode );
+    fgTie("/sim/vr/swapchain-mode",
+          this, &VRManager::getSwapchainMode, &VRManager::setSwapchainMode );
+}
+
+bool VRManager::getEnabled() const
+{
+    return _enabled;
+}
+
+void VRManager::setEnabled(bool enabled)
+{
+    if (_enabled == enabled)
+        return;
+    if (enabled) {
+        osgViewer::View *view = globals->get_renderer()->getView();
+        if (view) {
+            setViewer(globals->get_renderer()->getViewerBase());
+            view->apply(this);
+            if (_settings->present())
+                _enabled = true;
+        }
+    } else {
+        // FIXME uninit
+        //_enabled = false;
+    }
+}
+
+const char * VRManager::getMirrorMode() const
+{
+    osgXR::MirrorSettings &settings = _settings->getMirrorSettings();
+    osgXR::MirrorSettings::MirrorMode mirrorMode = settings.getMirrorMode();
+
+    switch (mirrorMode) {
+    case osgXR::MirrorSettings::MIRROR_AUTOMATIC:
+        return "AUTOMATIC";
+    case osgXR::MirrorSettings::MIRROR_NONE:
+        return "NONE";
+    case osgXR::MirrorSettings::MIRROR_SINGLE:
+        switch (settings.getMirrorViewIndex())
+        {
+        case 0:
+            return "LEFT";
+        case 1:
+            return "RIGHT";
+        }
+        break;
+    case osgXR::MirrorSettings::MIRROR_LEFT_RIGHT:
+        return "LEFT_RIGHT";
+    }
+    return "UNKNOWN";
+}
+
+void VRManager::setMirrorMode(const char * mode)
+{
+    osgXR::MirrorSettings::MirrorMode mirrorMode = osgXR::MirrorSettings::MIRROR_AUTOMATIC;
+    int viewIndex = -1;
+
+    if (strcmp(mode, "AUTOMATIC") == 0) {
+        mirrorMode = osgXR::MirrorSettings::MIRROR_AUTOMATIC;
+    } else if (strcmp(mode, "NONE") == 0) {
+        mirrorMode = osgXR::MirrorSettings::MIRROR_NONE;
+    } else if (strcmp(mode, "LEFT") == 0) {
+        mirrorMode = osgXR::MirrorSettings::MIRROR_SINGLE;
+        viewIndex = 0;
+    } else if (strcmp(mode, "RIGHT") == 0) {
+        mirrorMode = osgXR::MirrorSettings::MIRROR_SINGLE;
+        viewIndex = 1;
+    } else if (strcmp(mode, "LEFT_RIGHT") == 0) {
+        mirrorMode = osgXR::MirrorSettings::MIRROR_LEFT_RIGHT;
+    }
+
+    _settings->getMirrorSettings().setMirror(mirrorMode, viewIndex);
+}
+
+const char * VRManager::getVRMode() const
+{
+    osgXR::Settings::VRMode vrMode = _settings->getVRMode();
+
+    switch (vrMode) {
+    case osgXR::Settings::VRMODE_AUTOMATIC:
+        return "AUTOMATIC";
+    case osgXR::Settings::VRMODE_SLAVE_CAMERAS:
+        return "SLAVE_CAMERAS";
+    case osgXR::Settings::VRMODE_SCENE_VIEW:
+        return "SCENE_VIEW";
+    }
+    return "NONE";
+}
+
+void VRManager::setVRMode(const char * mode)
+{
+    osgXR::Settings::VRMode vrMode = osgXR::Settings::VRMODE_AUTOMATIC;
+
+    if (strcmp(mode, "AUTOMATIC") == 0) {
+        vrMode = osgXR::Settings::VRMODE_AUTOMATIC;
+    } else if (strcmp(mode, "SLAVE_CAMERAS") == 0) {
+        vrMode = osgXR::Settings::VRMODE_SLAVE_CAMERAS;
+    } else if (strcmp(mode, "SCENE_VIEW") == 0) {
+        vrMode = osgXR::Settings::VRMODE_SCENE_VIEW;
+    }
+
+    _settings->setVRMode(vrMode);
+}
+
+const char * VRManager::getSwapchainMode() const
+{
+    osgXR::Settings::SwapchainMode swapchainMode = _settings->getSwapchainMode();
+
+    switch (swapchainMode) {
+    case osgXR::Settings::SWAPCHAIN_AUTOMATIC:
+        return "AUTOMATIC";
+    case osgXR::Settings::SWAPCHAIN_MULTIPLE:
+        return "MULTIPLE";
+    case osgXR::Settings::SWAPCHAIN_SINGLE:
+        return "SINGLE";
+    }
+    return "NONE";
+}
+
+void VRManager::setSwapchainMode(const char * mode)
+{
+    osgXR::Settings::SwapchainMode swapchainMode = osgXR::Settings::SWAPCHAIN_AUTOMATIC;
+
+    if (strcmp(mode, "AUTOMATIC") == 0) {
+        swapchainMode = osgXR::Settings::SWAPCHAIN_AUTOMATIC;
+    } else if (strcmp(mode,"MULTIPLE") == 0) {
+        swapchainMode = osgXR::Settings::SWAPCHAIN_MULTIPLE;
+    } else if (strcmp(mode,"SINGLE") == 0) {
+        swapchainMode = osgXR::Settings::SWAPCHAIN_SINGLE;
+    }
+
+    _settings->setSwapchainMode(swapchainMode);
+}
+
+void VRManager::doCreateView(osgXR::View *xrView)
+{
+    // Construct a property tree for the camera
+    SGPropertyNode_ptr camNode = new SGPropertyNode;
+    WindowBuilder *windowBuilder = WindowBuilder::getWindowBuilder();
+    setValue(camNode->getNode("window/name", true),
+             windowBuilder->getDefaultWindowName());
+
+    // Build a camera
+    CameraGroup *cgroup = CameraGroup::getDefault();
+    CameraInfo *info = cgroup->buildCamera(camNode);
+
+    // Notify osgXR about the new compositor's scene slave cameras
+    if (info) {
+        _camInfos[xrView] = info;
+        _xrViews[info] = xrView;
+        info->reloadCompositorCallback = _reloadCompositorCallback;
+
+        postReloadCompositor(cgroup, info);
+    }
+}
+
+void VRManager::doDestroyView(osgXR::View *xrView)
+{
+    auto it = _camInfos.find(xrView);
+    if (it != _camInfos.end()) {
+        osg::ref_ptr<CameraInfo> info = (*it).second;
+        _camInfos.erase(it);
+
+        CameraGroup *cgroup = CameraGroup::getDefault();
+        cgroup->removeCamera(info.get());
+    }
+}
+
+void VRManager::preReloadCompositor(CameraGroup *cgroup, CameraInfo *info)
+{
+    osgXR::View *xrView = _xrViews[info];
+
+    auto& passes = info->compositor->getPassList();
+    for (auto& pass: passes)
+        if (pass->type == "scene")
+            xrView->removeSlave(pass->camera);
+}
+
+void VRManager::postReloadCompositor(CameraGroup *cgroup, CameraInfo *info)
+{
+    osgXR::View *xrView = _xrViews[info];
+
+    auto& passes = info->compositor->getPassList();
+    for (auto& pass: passes)
+        if (pass->type == "scene")
+            xrView->addSlave(pass->camera);
+}
+
+}

src/Viewer/fg_os_osgviewer.cxx
@@ -44,10 +44,6 @@
 #include <osgViewer/Viewer>
 #include <osgViewer/GraphicsWindow>
 
-#ifdef ENABLE_OSGXR
-#include <osgXR/osgXR>
-#endif
-
 #include <Scenery/scenery.hxx>
 #include <Main/fg_os.hxx>
 #include <Main/fg_props.hxx>
@@ -56,6 +52,7 @@
 #include "renderer.hxx"
 #include "CameraGroup.hxx"
 #include "FGEventHandler.hxx"
+#include "VRManager.hxx"
 #include "WindowBuilder.hxx"
 #include "WindowSystemAdapter.hxx"
 #include <Main/sentryIntegration.hxx>
@@ -299,14 +296,6 @@ void fgOSOpenWindow(bool stencil)
         // The viewer won't start without some root.
         viewer->setSceneData(new osg::Group);
         globals->get_renderer()->setView(viewer.get());
-
-#ifdef ENABLE_OSGXR
-        // Set up OpenXR
-        osgXR::setupViewerDefaults(viewer, "FlightGear",
-                                   FLIGHTGEAR_MAJOR_VERSION << 16 |
-                                   FLIGHTGEAR_MINOR_VERSION << 8 |
-                                   FLIGHTGEAR_PATCH_VERSION);
-#endif
     }
 }
 SGPropertyNode* simHost = 0, *simFrameCount, *simTotalHostTime, *simFrameResetCount, *frameWait;
@@ -340,6 +329,13 @@ void fgOSResetProperties()
     fgTie("/sim/rendering/osg-displaysettings/double-buffer", displaySettings, &DisplaySettings::getDoubleBuffer, &DisplaySettings::setDoubleBuffer );
     fgTie("/sim/rendering/osg-displaysettings/depth-buffer", displaySettings, &DisplaySettings::getDepthBuffer, &DisplaySettings::setDepthBuffer );
     fgTie("/sim/rendering/osg-displaysettings/rgb", displaySettings, &DisplaySettings::getRGB, &DisplaySettings::setRGB );
+
+#ifdef ENABLE_OSGXR
+    fgSetBool("/sim/vr/built", true);
+    VRManager::instance()->resetProperties();
+#else
+    fgSetBool("/sim/vr/built", false);
+#endif
 }
 
 

commit 1594728f158f8f437c23031c04d06aa009e38de9

VR: Drop VRResetProperties() declaration
Remove the declaration of VRResetProperties() in VRManager.hxx, as it
was never used.

Fixes: 24cb7b2d3079 ("VR: Implement a VRManager class")

src/Viewer/VRManager.hxx
@@ -32,12 +32,8 @@
 
 #include <map>
 
-#endif
-
 namespace flightgear
 {
-
-#ifdef ENABLE_OSGXR
 
 class VRManager : public osgXR::Manager
 {
@@ -112,10 +108,8 @@ class VRManager : public osgXR::Manager
         osg::ref_ptr<ReloadCompositorCallback> _reloadCompositorCallback;
 };
 
-#endif // ENABLE_OSGXR
-
-void VRResetProperties();
-
 }
+
+#endif // ENABLE_OSGXR
 
 #endif