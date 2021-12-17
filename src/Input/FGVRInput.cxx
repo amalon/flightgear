amalon/flightgear

commit 32e5bc84a6e527245f8616cf9e82bfd41492f323

FGVRInput: Implement stack of modes per subaction
Implement a stack of interaction modes per subaction. Subaction
functions now exist to push a mode, pop modes in front of some mode, and
clear modes, but they cannot be invoked from fgcommands yet.

All modes in the stack are updated.

src/Input/FGVRInput.cxx
@@ -43,8 +43,7 @@ using flightgear::VRManager;
 
 FGVRInput::Subaction::Subaction(osgXR::Manager *manager,
                                 const std::string &path) :
-    osgXR::Subaction(manager, path),
-    _curMode(nullptr)
+    osgXR::Subaction(manager, path)
 {
 }
 
@@ -60,20 +59,43 @@ const char *FGVRInput::Subaction::getPresetMode(const char *id)
     return _modesNode->getStringValue(id, nullptr);
 }
 
-void FGVRInput::Subaction::setMode(FGVRInput::Mode *mode)
+void FGVRInput::Subaction::pushMode(FGVRInput::Mode *mode)
 {
-    if (_curMode)
-        _curMode->deactivate(this);
-    _curMode = mode;
-    if (_curMode)
-        _curMode->activate(this);
+    _modeStack.push_back(mode);
+    mode->activate(this);
+}
+
+unsigned int FGVRInput::Subaction::popModesFront(FGVRInput::Mode *keepMode)
+{
+    int index = -1;
+    for (unsigned int i = 0; i < _modeStack.size(); ++i)
+        if (_modeStack[i] == keepMode)
+            index = i;
+    // not found?
+    if (index < 0)
+        return 0;
+
+    // pop off end leaving keepMode at the top
+    unsigned int ret = _modeStack.size() - (index + 1);
+    while ((int)_modeStack.size() > index + 1) {
+        _modeStack[_modeStack.size() - 1]->deactivate(this);
+        _modeStack.pop_back();
+    }
+    return ret;
+}
+
+void FGVRInput::Subaction::clearModes()
+{
+    while (!_modeStack.empty())
+        _modeStack.back()->deactivate(this);
 }
 
 void FGVRInput::Subaction::update(double dt)
 {
-    // If there is an active mode, allow it to poll for changes
-    if (_curMode)
-        _curMode->update(this, dt);
+    // Allow active modes to poll for changes
+    // Note this may change the mode stack
+    for (unsigned int i = 0; i < _modeStack.size(); ++i)
+        _modeStack[i]->update(this, dt);
 }
 
 // FGVRInput::ActionSet
@@ -953,7 +975,7 @@ void FGVRInput::_remove(bool all)
 
     // Clean up interaction modes
     for (auto &subactionPair: _subactions)
-        subactionPair.second->setMode(nullptr);
+        subactionPair.second->clearModes();
     for (auto &modePair: _modes)
         delete modePair.second;
 }
@@ -1023,7 +1045,7 @@ void FGVRInput::init()
         const char *defaultMode = subaction->getPresetMode("default");
         auto it = _modes.find(defaultMode);
         if (it != _modes.end())
-            subaction->setMode((*it).second);
+            subaction->pushMode((*it).second);
     }
 }
 

src/Input/FGVRInput.hxx
@@ -98,14 +98,17 @@ public:
             /// Get a preset mode.
             const char *getPresetMode(const char *id);
 
-            /// Set the current mode this subaction should be using.
-            void setMode(Mode *mode);
+            /// Push a mode in front.
+            void pushMode(Mode *mode);
 
-            /// Get the current mode this subaction should be using.
-            Mode *getMode(Mode *mode)
-            {
-                return _curMode;
-            }
+            /**
+             * Pop modes in front of @a keepMode.
+             * @return Number of nodes removed.
+             */
+            unsigned int popModesFront(Mode *keepMode);
+
+            /// Clear all modes.
+            void clearModes();
 
             /// Regular update of subaction.
             void update(double dt);
@@ -115,8 +118,8 @@ public:
             /// Preset modes node.
             SGPropertyNode_ptr _modesNode;
 
-            /// Current mode this subaction should be using
-            Mode *_curMode;
+            /// Stack of modes in use by the subaction.
+            std::vector<Mode *> _modeStack;
     };
 
 private:

commit 501eb466b120308515b663bc9020577ab9a07e3d

FGVRInput: Fix hang in Subaction::clearModes()
Clearing of a subaction mode stack repeatedly deactivates the top mode
without ever popping it. This is exposed now I'm able to run FlightGear
with the Monado OpenXR runtime which doesn't hang on instance
destruction (like SteamVR does).

Pop the top mode from the stack each time as originally intended.

Fixes: 32e5bc84a6e5 ("FGVRInput: Implement stack of modes per subaction")

src/Input/FGVRInput.cxx
@@ -86,8 +86,10 @@ unsigned int FGVRInput::Subaction::popModesFront(FGVRInput::Mode *keepMode)
 
 void FGVRInput::Subaction::clearModes()
 {
-    while (!_modeStack.empty())
+    while (!_modeStack.empty()) {
         _modeStack.back()->deactivate(this);
+        _modeStack.pop_back();
+    }
 }
 
 void FGVRInput::Subaction::update(double dt)